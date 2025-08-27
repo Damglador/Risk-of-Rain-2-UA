e8455aa7c77d0b241f9169bb55699e92a50f5d3d
Damglador <vse.stopchanskyi@gmail.com>
2025-08-27 21:20:03 +0200

    Fix the build-output script

--- build-output.sh
+++ build-output.sh
@@ -2,18 +2,24 @@
 
 # Зклеює купу json файлів з наданої директорії в один
 # Люта милиця, треба перевіряти вихідний файл на помилки
+# Файли з blacklist не будуть враховані
 
 input_folder="$1"
 output_file="output-ukrainian.json"
+blacklist=(
+    "$output_file"
+    language.json
+    cu8.json
+)
 
 echo "{
-	\"strings\":
-	{" > "$output_file"
+	\"strings\": {" > "$output_file"
 
 for f in "$input_folder"/*.json; do
-    [ "$(basename "$f")" = "$output_file" ] && continue
-    [ "$(basename "$f")" = "language.json" ] && continue
-    grep "		.*" "$f" >> "$output_file"
+    for b in "${blacklist[@]}"; do
+        [ "$(basename "$f")" == "$b" ] && continue 2
+    done
+    grep -oP '"(\\.|[^"\\])*"\s*:\s*"(\\.|[^"\\])*"' "$f" | sed 's/^/		/' >> "$output_file"
 done
 
 sed -i 's/"*"*"*"$/&,/g' "$output_file"
