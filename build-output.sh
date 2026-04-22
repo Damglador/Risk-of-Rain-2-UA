d6b2e2bb6546c634a4849a24a32c370723705fb1
Damglador <vse.stopchanskyi@gmail.com>
2026-04-22 23:28:02 +0200

    Change formatting to use 2 spaces as tabs

--- build-output.sh
+++ build-output.sh
@@ -7,23 +7,23 @@
 input_folder="$1"
 output_file="output-ukrainian.json"
 blacklist=(
-    "$output_file"
-    language.json
-    cu8.json
-    DLC3.json
+  "$output_file"
+  language.json
+  cu8.json
+  DLC3.json
 )
 
 echo "{
-	\"strings\": {" > "$output_file"
+  \"strings\": {" > "$output_file"
 
 for f in "$input_folder"/*.json; do
-    for b in "${blacklist[@]}"; do
-        [ "$(basename "$f")" == "$b" ] && continue 2
-    done
-    grep -oP '"(\\.|[^"\\])*"\s*:\s*"(\\.|[^"\\])*"' "$f" | sed 's/^/		/' >> "$output_file"
+  for b in "${blacklist[@]}"; do
+    [ "$(basename "$f")" == "$b" ] && continue 2
+  done
+  grep -oP '"(\\.|[^"\\])*"\s*:\s*"(\\.|[^"\\])*"' "$f" | sed 's/^/		/' >> "$output_file"
 done
 
 sed -i 's/"*"*"*"$/&,/g' "$output_file"
 
-echo "	}
+echo "  }
 }" >> "$output_file"
