b0cc52968960d5aaf38c0bf9a625a3c99198bfcf
Damglador <vse.stopchanskyi@gmail.com>
2026-05-31 19:04:21 +0200

    Use double spaces for tabulation everywhere

--- lang_mods/combine.sh
+++ lang_mods/combine.sh
@@ -8,7 +8,7 @@ output_file="${2:-"output-ukrainian.json"}"
 blacklist=("$output_file")
 
 echo "{
-	\"uk\": {" > "$output_file"
+  \"uk\": {" > "$output_file"
 
 for f in "$input_folder"/*; do
   if ! file "$f" | grep -q "text"; then
@@ -17,7 +17,7 @@ for f in "$input_folder"/*; do
   for b in "${blacklist[@]}"; do
       [ "$(basename "$f")" == "$b" ] && continue 2
   done
-  grep -oP '"(\\.|[^"\\])*"\s*:\s*"(\\.|[^"\\])*"' "$f" | sed 's/^/		/' >> "$output_file"
+  grep -oP '"(\\.|[^"\\])*"\s*:\s*"(\\.|[^"\\])*"' "$f" | sed 's/^/    /' >> "$output_file"
 done
 
 sed -i 's/"*"*"*"$/&,/g' "$output_file"
