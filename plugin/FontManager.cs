b96ba2354774c86caee37acca00908fc10fcd7ee
Damglador <vse.stopchanskyi@gmail.com>
2026-06-24 16:38:14 +0200

    Add custom font material for holograms

--- plugin/FontManager.cs
+++ plugin/FontManager.cs
@@ -10,11 +10,16 @@ public class FontManager
 {
     public static AssetBundle assetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Risk_of_Rain_2_Ukrainian.ukrainianfont"));
     public static TMP_FontAsset fontBomb;
+    public static Material dropshadowMaterial;
+    public static Material hologramMaterial;
     public static TMP_FontAsset fontBombDefault;
 
     public FontManager()
     {
         fontBomb = assetBundle.LoadAsset<TMP_FontAsset>("Assets/BOMBARD+Ukrainian.asset");
+        dropshadowMaterial = assetBundle.LoadAsset<Material>("Assets/BOMBARD+Ukrainian.dropshadow.mat");
+        hologramMaterial = assetBundle.LoadAsset<Material>("Assets/BOMBARD+Ukrainian.hologram.mat");
+
         fontBombDefault = Resources.Load<TMP_FontAsset>("TmpFonts/Bombardier/tmpBombDropShadow");
 
         On.RoR2.UI.HGTextMeshProUGUI.OnCurrentLanguageChanged += HGTextMeshProUGUI_OnCurrentLanguageChanged;
@@ -26,7 +31,13 @@ public class FontManager
     {
         orig(self);
         if (self.font == fontBombDefault)
+        {
             self.font = fontBomb;
+            if (self is TextMeshProUGUI)
+                self.fontMaterial = dropshadowMaterial;
+            if (self is TextMeshPro)
+                self.fontMaterial = hologramMaterial;
+        }
     }
 
     public void HGTextMeshProUGUI_OnCurrentLanguageChanged(On.RoR2.UI.HGTextMeshProUGUI.orig_OnCurrentLanguageChanged orig)
