95ec1d089dbfc99a5542cb1a85e59b3119311f83
Damglador <vse.stopchanskyi@gmail.com>
2026-06-24 07:43:07 +0200

    Code clean-up

--- plugin/FontManager.cs
+++ plugin/FontManager.cs
@@ -8,18 +8,20 @@ namespace Risk_of_Rain_2_Ukrainian;
 
 public class FontManager
 {
+    public static AssetBundle assetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Risk_of_Rain_2_Ukrainian.ukrainianfont"));
+    public static TMP_FontAsset fontBomb;
+    public static TMP_FontAsset fontBombDefault;
+
     public FontManager()
     {
         fontBomb = assetBundle.LoadAsset<TMP_FontAsset>("Assets/BOMBARD+Ukrainian.asset");
         fontBombDefault = Resources.Load<TMP_FontAsset>("TmpFonts/Bombardier/tmpBombDropShadow");
-        fontDamageDefault = Resources.Load<GameObject>("Prefabs/Effects/BearProc").transform.Find("TextCamScaler/TextRiser/TextMeshPro").gameObject.GetComponent<TextMeshPro>().font;
 
-        On.RoR2.UI.HGTextMeshProUGUI.OnCurrentLanguageChanged += new On.RoR2.UI.HGTextMeshProUGUI.hook_OnCurrentLanguageChanged(this.HGTextMeshProUGUI_OnCurrentLanguageChanged);
-        new Hook(typeof(TextMeshProUGUI).GetMethod("LoadFontAsset", (BindingFlags)(-1)), new Action<Action<TextMeshProUGUI>, TextMeshProUGUI>(delegate (Action<TextMeshProUGUI> orig, TextMeshProUGUI self)
+        On.RoR2.UI.HGTextMeshProUGUI.OnCurrentLanguageChanged += HGTextMeshProUGUI_OnCurrentLanguageChanged;
+        _ = new Hook(typeof(TextMeshProUGUI).GetMethod("LoadFontAsset", (BindingFlags)(-1)), new Action<Action<TextMeshProUGUI>, TextMeshProUGUI>(delegate (Action<TextMeshProUGUI> orig, TextMeshProUGUI self)
         {
             orig(self);
-            bool flag = self.font == fontBombDefault;
-            if (flag)
+            if (self.font == fontBombDefault)
                 self.font = fontBomb;
         }));
     }
@@ -27,13 +29,7 @@ public class FontManager
     public void HGTextMeshProUGUI_OnCurrentLanguageChanged(On.RoR2.UI.HGTextMeshProUGUI.orig_OnCurrentLanguageChanged orig)
     {
         orig.Invoke();
-        bool flag = fontBomb;
-        if (flag)
+        if (fontBomb)
             RoR2.UI.HGTextMeshProUGUI.defaultLanguageFont = fontBomb;
     }
-    public static AssetBundle assetBundle = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("Risk_of_Rain_2_Ukrainian.ukrainianfont"));
-    public static TMP_FontAsset fontBomb;
-    public static TMP_FontAsset fontBombDefault;
-    public static TMP_FontAsset fontDamage;
-    public static TMP_FontAsset fontDamageDefault;
 }
